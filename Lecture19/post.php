b325b8d46e0c24b21ba0c6cd06cb6d2cb9fd5f2b
ELarWSU <181546842+ELarWSU@users.noreply.github.com>
2025-10-21 09:33:36 -0500

    lab02 started, lecture notes for lecture 19

---
+++ Lecture19/post.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "var_dump(5)";
+echo var_dump(5);
+?>
+
+</body>
+</html>
---
+++ Lecture19/welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name = "author" content="your name">
+        <meta Title = "php form">
+    </head>
+    <body>
+        <?php
+            phpinfo();
+        ?>
+    </body>
+</html>
